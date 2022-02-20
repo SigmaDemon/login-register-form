00a2fbb431e07b2b21eec0b5e0ad72dfb36c693b
SigmaDemon <sebastianpoiana2015@gmail.com>
2022-02-20 18:58:30 +0000

    Uploaded Project Files
    The C# .NET files are being uploaded to the GitHub Repository for the form.

---
+++ MaestroCorporan/MaestroCorporan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MaestroCorporan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
